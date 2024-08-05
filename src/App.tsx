81e6a9426db21c2ebab834b6f2259413d22a134b
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-08-05 17:51:25 +0200

    organize and name the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
 import MovieList from './components/MovieList';
-import styled from 'styled-components';
-
-const AppContainer = styled.div`
-  text-align: center;
-`;
-
-const AppHeader = styled.header`
-  background-color: #282c34;
-  min-height: 10vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`;
+import './App.css'; 
 
 const App: React.FC = () => {
   return (
-    <AppContainer>
-      <AppHeader>
-        <h1>Video Listing</h1>
-      </AppHeader>
+    <div className="app-container">
+      <header className="app-header">
+        <h1>Seshat</h1>
+      </header>
       <MovieList />
-    </AppContainer>
+    </div>
   );
 };
 
